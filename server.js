0c98dbd864ddbc8a90467109d00ea73c4c8c53eb
sanikapol <50167925+sanikapol@users.noreply.github.com>
2020-10-03 12:33:37 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs')
 
 app.use('/',express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 380
-        },
-        {
-            title: 'Groceries',
-            budget: 100
-        },
-    ]
-};
 
 
 app.get('/hello', (req,res) => {
     res.send("Hello World");
 });
 
-app.get('/budget', (req,res) => {
-    res.json(budget);
+app.get('/budget', (req,res) => { 
+    try{
+        const dataBuffer = fs.readFileSync('budget.json')
+        console.log(dataBuffer)
+        const budget = JSON.parse(dataBuffer);
+        console.log(budget)
+        res.json(budget);
+    }catch (err){
+        console.log(err)
+        res.send("Something went wrong")
+    }
+    
 });
 
 app.listen(port, () => {
